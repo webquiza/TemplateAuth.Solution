e803d0bf6eea6781deb24a6f5aff45b638c6b1f8
Carlos Urquiza <webquiza@gmail.com>
2021-10-27 19:09:12 -0700

    add functionality up to c# week 9 lesson 4

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -2,16 +2,16 @@ using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
-    public class Item
+  public class Item
+  {
+    public Item()
     {
-        public Item()
-        {
-            this.JoinEntities = new HashSet<CategoryItem>();
-        }
-
-        public int ItemId { get; set; }
-        public string Description { get; set; }
-
-        public virtual ICollection<CategoryItem> JoinEntities { get;}
+      this.JoinEntities = new HashSet<CategoryItem>();
     }
+
+    public int ItemId { get; set; }
+    public string Description { get; set; }
+
+    public virtual ICollection<CategoryItem> JoinEntities { get;}
+  }
 }
